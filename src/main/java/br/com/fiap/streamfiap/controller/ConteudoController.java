c2307f8aafe6b23a6b38ed7b936270232db9af67
MUnizZz526 <munizcaua88@gmail.com>
2026-09-04 09:15:25 -0300

    fix: bug07 - corrige busca de conteudos por categoria

--- src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
+++ src/main/java/br/com/fiap/streamfiap/controller/ConteudoController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
@@ -37,13 +37,7 @@ public class ConteudoController {
     // GET /api/conteudos/categoria/{categoria} - Buscar por categoria
     @GetMapping("/categoria/{categoria}")
     public List<Conteudo> listarPorCategoria(@PathVariable String categoria) {
-        List<Conteudo> resultado = new ArrayList<>();
-        for (Conteudo c : conteudoRepository.findAll()) {
-            if (c.getCategoria() == categoria) {
-                resultado.add(c);
-            }
-        }
-        return resultado;
+        return conteudoRepository.findByCategoria(categoria);
     }
 
     // GET /api/conteudos/{id}/preco-promocional - Preço com promoção
