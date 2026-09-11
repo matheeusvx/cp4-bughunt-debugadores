98485fbb541f98a3aee2fb554091694ffaf38c53
Rafael Ferreira <rafaelferreirammartins@gmail.com>
2026-09-11 15:51:21 -0300

    refactor: clean08 - remove comentario enganoso em debitarCreditos
    O comentario dizia que o metodo adiciona o valor aos creditos, mas ele subtrai. O nome do metodo ja explica o que ele faz.

--- src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -32,7 +32,6 @@ public class Usuario {
     }
 
     public void debitarCreditos(double valor) {
-        // adiciona o valor aos créditos do usuário
         this.creditos = this.creditos - valor;
     }
 
