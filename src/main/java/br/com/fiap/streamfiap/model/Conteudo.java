6d05bb17d2add98e2ccae722d5bbc75b3a7710e7
VictorNMortari <victornmortari@gmail.com>
2026-09-11 09:20:18 -0300

    refactor: clean-code torna calculo de preco abstrato no conteudo

--- src/main/java/br/com/fiap/streamfiap/model/Conteudo.java
+++ src/main/java/br/com/fiap/streamfiap/model/Conteudo.java
@@ -32,9 +32,7 @@ public abstract class Conteudo {
         this.disponivel = disponivel;
     }
 
-    public double calcularPrecoAluguel() {
-        return 9.90;
-    }
+    public abstract double calcularPrecoAluguel();
 
     public double calcularPrecoPromocional() {
         if (this instanceof Promocionavel) {
